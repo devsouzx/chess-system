a48fe55c72728169c306d5dae864ecb63db2ebb7
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-05-20 19:32:35 -0300

    Moving pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,6 +23,27 @@ public class ChessMatch {
     return mat;
   }
 
+  public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+    Position source = sourcePosition.toPosition();
+    Position target = targetPosition.toPosition();
+    validateSourcePosition(source);
+    Piece capturedPiece = makeMove(source, target);
+    return (ChessPiece) capturedPiece;
+  }
+
+  private void validateSourcePosition(Position position) {
+    if (!board.thereIsAPiece(position)) {
+      throw new ChessException("There is no piece on source position!");
+    }
+  }
+
+  private Piece makeMove(Position source, Position target) {
+    Piece p = board.removePiece(source);
+    Piece capturdePiece = board.removePiece(target);
+    board.placePiece(p, target);
+    return capturdePiece;
+  }
+
   private void placeNewPiece(char column, int row, Piece piece) {
     board.placePiece(piece, new ChessPosition(column, row).toPosition());
   }
